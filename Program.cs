9f0b3f3991de9659185f62889530de27f03b96df
Igor Baranov <eltery@mail.ru>
2022-12-07 18:05:32 +0300

    Palindrome task done

--- Program.cs
+++ Program.cs
@@ -1,4 +1,37 @@
-﻿/*
+﻿bool Palindrome (int number)
+
+{   int originalNumber = number;
+    int reverse = 0;
+    
+    while(originalNumber > 0)
+    {
+        int lastDigit = originalNumber % 10;
+        reverse = reverse * 10 + lastDigit;
+        originalNumber = originalNumber / 10;
+    }
+
+    if(number == reverse) 
+        return true;
+    else
+        return false;
+
+}
+
+Console.Write("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+bool result = Palindrome(userNumber);
+//Console.WriteLine(result);
+
+if(result == true)
+{
+    Console.WriteLine("Ваше число - палиндром.");
+}
+else
+{
+    Console.WriteLine("Ваше число не является палиндромом!");
+}
+
+/*
 double FindDistance(double xA, double yA, double zA, double xB, double yB, double zB)
 {
     double distance = Math.Sqrt(Math.Pow(xA - xB, 2) + Math.Pow(yA - yB, 2) + Math.Pow(zA - zB, 2));
@@ -24,7 +57,7 @@ double dist = Math.Round(FindDistance(xA, yA, zA, xB, yB, zB),2);
 Console.WriteLine($"Расстояние между точкой А с координатами ({xA}, {xB}, {yB}) и точкой  B с координатами ({xB}, {yB}, {zB}) = {dist}");
 */
 
-
+/*
 void FindCube(int N)
 {
     int number = 1;
@@ -40,4 +73,4 @@ Console.Write("Введите число: ");
 int cubeNumber = Convert.ToInt32(Console.ReadLine());
 
 FindCube(cubeNumber);
-
+*/
