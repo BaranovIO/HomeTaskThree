6ccd6f5589655518c8d71461c996df5244e11b40
Igor Baranov <eltery@mail.ru>
2022-12-07 17:05:32 +0300

    Find cube of number done

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿/*
 double FindDistance(double xA, double yA, double zA, double xB, double yB, double zB)
 {
     double distance = Math.Sqrt(Math.Pow(xA - xB, 2) + Math.Pow(yA - yB, 2) + Math.Pow(zA - zB, 2));
@@ -22,3 +22,22 @@ double zB = Convert.ToDouble(Console.ReadLine());
 double dist = Math.Round(FindDistance(xA, yA, zA, xB, yB, zB),2);
 
 Console.WriteLine($"Расстояние между точкой А с координатами ({xA}, {xB}, {yB}) и точкой  B с координатами ({xB}, {yB}, {zB}) = {dist}");
+*/
+
+
+void FindCube(int N)
+{
+    int number = 1;
+
+    while (number <= N)
+    {
+        Console.Write(Math.Pow(number, 3) + " ");
+        number++;
+    }
+}
+
+Console.Write("Введите число: ");
+int cubeNumber = Convert.ToInt32(Console.ReadLine());
+
+FindCube(cubeNumber);
+
