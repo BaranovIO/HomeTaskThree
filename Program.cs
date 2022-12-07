22fdea90d0682d826d7c55361147b0eed5f97d6c
Igor Baranov <eltery@mail.ru>
2022-12-07 16:50:04 +0300

    Distance between 2 dots in 3D done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+double FindDistance(double xA, double yA, double zA, double xB, double yB, double zB)
+{
+    double distance = Math.Sqrt(Math.Pow(xA - xB, 2) + Math.Pow(yA - yB, 2) + Math.Pow(zA - zB, 2));
+    return distance;
+}
+
+Console.Write("Введите координату х первого числа ");
+double xA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату y первого числа ");
+double yA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату z первого числа ");
+double zA = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координату х второго числа ");
+double xB = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату y второго числа ");
+double yB = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату z второго числа ");
+double zB = Convert.ToDouble(Console.ReadLine());
+
+double dist = Math.Round(FindDistance(xA, yA, zA, xB, yB, zB),2);
+
+Console.WriteLine($"Расстояние между точкой А с координатами ({xA}, {xB}, {yB}) и точкой  B с координатами ({xB}, {yB}, {zB}) = {dist}");
